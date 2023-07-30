3b880b756229b077d8d7baef35eb15bad9cf3889
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2023-07-30 17:33:32 +0700

    add_*

--- Btvn/Btvn_bai4/Btvn_bai4/Program.cs
+++ Btvn/Btvn_bai4/Btvn_bai4/Program.cs
@@ -1,5 +1,4 @@
-﻿//int n = int.Parse(Console.ReadLine());
-Console.Write("Nhap so phan tu: ");
+﻿Console.Write("Nhap so phan tu: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] arr = new int[n];
 
@@ -7,11 +6,11 @@ for (int i = 0; i < n; i++)
 {
     Console.Write("Nhap phan tu: ");
     arr[i] = Convert.ToInt32(Console.ReadLine()) ;
-    //arr[i] = int.Parse(Console.ReadLine());
-
 }
+
 int maxValue = arr[0];
 int minValue = arr[1];
+
 for (int i = 0; i < n; i++)
 {
     if (arr[i] > maxValue)
