2c628c54d93e1dd5d6c9897c57327c3b9a21c6b1
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2023-07-30 16:18:58 +0700

    add1

---
+++ Btvn/Btvn_bai1/Btvn_bai1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+///MAX cua Mang mot chieu
+///
+/*Console.WriteLine("\n Nhap so phan tu cua mang: ");
+int n =Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Nhap phan tu thu: " + i);
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+int max = arr[0];
+for (int i = 0;i < n; i++)
+{
+    if (max < arr[i])
+        max = arr[i];
+}
+
+Console.WriteLine("Max is: " +  max);*/
+
+//MAX cua mang hai chieu
+
+/*int[,] arr2D =
+{
+    {1,4,5,6,7,8, },
+    {1,4,5,6,8,9 }
+};
+int max = arr2D[0, 0];
+
+for (int i = 0; i < arr2D.GetLength(0); i++)
+{
+    for (int j = 0; j < arr2D.GetLength(1); j++)
+    {
+        if (arr2D[i,j] > max)
+        {
+            max = arr2D[i,j];
+        }
+    }
+}
+Console.WriteLine(max);*/
+
+//MAX cua mang ba chieu
+
+int[,,] arr3D =
+{
+    {
+        {2, 3 },
+        { 4, 5 },
+        { 6, 7 },
+        { 8, 9 },
+        { 9, 10 }
+     }
+};
+int max = arr3D[0, 0, 0];
+ for  (int i = 0; i < arr3D.GetLength(0); i++)
+{
+    for (int j = 0; j < arr3D.GetLength(1); j++)
+    {
+        for (int k = 0; k < arr3D.GetLength(2); k++)
+        {
+            if (arr3D[i, j, k] > max)
+            {
+                max = arr3D[i, j, k];
+            }
+        }
+    }
+}
+Console.WriteLine(max);
---
+++ study/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿//Dzoi
+
+//MANG 1 CHIEU
+/*int sum = 0;
+int[] a = { 1, 3, 5, 7, 9 };
+for (int i = 0; i < a.Length; i++)
+{
+    sum += a[i];
+    
+}
+Console.WriteLine(sum);*/
+//////////////////
+
+/*int[] nums = {10, 20, 30, 40,50 };
+int sum = 0;
+foreach (int num in nums)
+{
+    sum += num;
+}
+Console.WriteLine("Sum " + sum);*/
+////////////////////
+
+using System;
+/*
+int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; 
+int sum = 0;
+
+//duyet
+foreach (int num in nums)
+{
+    //kiem tra
+    if (num % 2 == 0)
+    {
+        sum += num;
+    }
+}
+Console.WriteLine("Sum is: " + sum);
+*/
+/////////////////
+
+/*int[] numbers = { 4, 6, 3, 7, 8 };
+Console.WriteLine(numbers[1]);
+numbers[1] = 3000;
+Console.WriteLine(numbers[1]);*/
+
+//MANG 2 CHIEU
+/*int sum = 0;
+int[,] arr = { 
+    { 1, 2, 3 },
+    { 4, 5, 6 } 
+};
+for (int i = 0; i < arr.GetLength(0); i++) 
+{ 
+    for (int j = 0; j < arr.GetLength(1); j++) 
+    {
+        sum += arr[i,j];
+    }
+}
+Console.WriteLine(sum);
+*/
+//MANG 3 CHIEU
+/*int[,,] array3D = new int[,,]
+{
+    {
+        { 1, 2, 3 }, 
+        { 4, 5, 6 } 
+    },
+    { 
+        { 7, 8, 9 },
+        { 10, 11, 12 } 
+    }
+}; 
+Console.WriteLine(array3D[0, 0, 0]); 
+Console.WriteLine(array3D[0, 1, 1]); 
+Console.WriteLine(array3D[1, 1, 1]);*/
+//Console.WriteLine(array3D[2, 0, 0])
+
+/*List<byte[]> list = new List<byte[]>();
+while (true)
+{
+    // allocate 1 GB of memory
+    byte[] array = new byte[1024 * 1024 * 1024];
+    list.Add(array);
+}*/
